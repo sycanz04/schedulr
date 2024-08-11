dbea48da8232244ef58b669049cbc168cc138e82
sycanz04 <aidenchan0397@gmail.com>
2024-08-11 13:09:38 +0800

    feat: basic info scraped

--- scripts/content.js
+++ scripts/content.js
@@ -1,14 +1,45 @@
-// console.log(document.querySelector("#WEEKLY_SCHED_HTMLAREA > tbody > tr:nth-child(1) > th:nth-child(1)")?.textContent)
-// classElement = console.log(document.querySelector("#DERIVED_REGFRM1_SS_TRANSACT_TITLE")?.textContent)
-// console.log(document.querySelector("#DERIVED_SSTSNAV_PERSON_NAME")?.textContent)
-// classElement = console.log(document.querySelector("#pthnavbca_MYFAVORITES")?.textContent)
+// Declare iframe
+iframeElement = document.querySelector("#ptifrmtgtframe");
 
-iframeElement = document.querySelector("#ptifrmtgtframe")
+if (iframeElement) {
+    // Access the iframe's content document
+    const iframeDocument = iframeElement.contentWindow.document.body;
 
-console.log(iframeElement.contentWindow.document.body.querySelectorAll("#DERIVED_SSTSNAV_PERSON_NAME"))
-console.log(iframeElement.contentWindow.document.body.querySelectorAll(".PSLEVEL3GRIDODDROW"))
-// if (classElement){
-//     console.log(classElement);
-// } else {
-//     console.log("Element not found.");
-// }
+    // Select elements in iframe
+    const name = iframeDocument.querySelector("#DERIVED_SSTSNAV_PERSON_NAME");
+    const day = iframeDocument.querySelectorAll("th.PSLEVEL3GRIDODDROW");
+    const classes = iframeDocument.querySelectorAll("td.PSLEVEL3GRIDODDROW");
+    
+    // Get the name details
+    if (name) {
+        const textContent = name.textContent;
+
+        console.log(textContent);
+    } else {
+        console.log("Name not found inside iframe");
+    }
+
+    // Get the dates
+    if (day.length > 0) {
+        day.forEach((element) => {
+            const dayText = element.textContent.split("\n");
+            const day = dayText[0].trim();
+            console.log(day)
+        })
+    }
+
+    // Get the class details
+    if (classes.length > 0) {
+        classes.forEach((element) => {
+            const spanElement = element.querySelector("span");
+            if (spanElement) {
+                const classesContent = spanElement.textContent;
+                console.log(classesContent)
+            }
+        });
+    } else {
+        console.log("No class elements found in side iframe")
+    }
+} else{
+    console.log("iframe not found")
+}
