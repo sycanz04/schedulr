ca3393a53cb40a499c3ce3ec2b3eebace68738e5
sycanz04 <aidenchan0397@gmail.com>
2024-10-30 16:26:46 +0800

    fix: support lecturer workflow

--- popup/popup.js
+++ popup/popup.js
@@ -507,151 +507,297 @@ END:VALARM`;
         return realDate;
     }
 
+    function procClassName(className) {
+        // console.log(className.trim());
+        splitClassName = className.trim().split(/\s+/);
+        // console.log(splitClassName);
+
+        let classCode = splitClassName[0] + splitClassName[1];
+        let classNameOnly = "";
+        for (let i = 2; i < splitClassName.length; i++) {
+            classNameOnly += splitClassName[i] + " ";
+        }
+
+        classNameOnly = classNameOnly.trim();
+
+        // console.log(classCode, ",", classNameOnly);
+        return {classCode, classNameOnly};
+    }
+
+    function procClassDetails(classDetails) {
+        splitClassDetails = classDetails.split(" - ");
+        // console.log(splitClassDetails);
+
+        classType = splitClassDetails[0].split(" ")[1];
+        classSect = splitClassDetails[1].split(" ")[2];
+        // console.log(splitClassType);
+
+        return {classType, classSect};
+    }
+
+    function procClassDates(classDates) {
+        // console.log(classDates);
+        let splitClassDates = classDates.trim().split(" - ");
+        // console.log(splitClassDates[0], splitClassDates[1]);
+
+        let startDate = splitClassDates[0].replace(/\//g, "-").split("-").reverse().join("-");
+        let endDate = splitClassDates[1].replace(/\//g, "-").split("-").reverse().join("-");
+
+        // console.log(startDate, ",", endDate);
+        return {startDate, endDate};
+    }
+
+    function procClassTimes(classTimes) {
+        // console.log(classTimes);
+        let splitClassTimes = classTimes.trim().split(" ");
+        // console.log(splitClassTimes[1], ",", splitClassTimes[3]);
+
+        function convertTimeFormat(timeStr) {
+            const [time, period] = timeStr.split(/(AM|PM)/);
+
+            let [hour, minute] = time.split(':');
+
+            if (period === 'AM' && parseInt(hour) < 10) {
+                hour = `0${hour}`;
+            } else if (period === 'AM' && parseInt(hour) > 9) {
+                hour = `${hour}`;
+            } else if (period === 'PM' && parseInt(hour) < 12) {
+                hour = parseInt(hour) + 12;
+            }
+
+            return `${hour}:${minute}:00+08:00`;
+        }
+
+        startTime = convertTimeFormat(splitClassTimes[1]);
+        endTime = convertTimeFormat(splitClassTimes[3]);
+        // console.log(convertTimeFormat(startTime), ",", convertTimeFormat(endTime));
+
+        return {startTime, endTime};
+    }
+
     // =============== End of helper functions ===============
 
     // =============== Web scrape workflow ===============
+    let classEvents = [];
+
     // Declare iframe
     const iframeElement = document.querySelector("#ptifrmtgtframe");
 
-    if (!iframeElement) {
-        console.error("iframe not found!");
-        window.alert("iframe not found!");
-        return;
+    const lectIndicator = document.querySelector("table.ptalNoPadding.ptalPgltAreaControlsIcon a#ptalPgltAreaHide");
+    
+    if (lectIndicator) {
+        // Do the lecturer's process
+        console.log("We got a lecturer here!");
+        lectFlow();
+    } else {
+        // Do the student's process
+        studentFlow();
     }
 
-    // Access the iframe's content document
-    const iframeDocument = iframeElement.contentWindow.document.body;
+    function lectFlow() {
+        // Access the iframe's content document
+        const iframeDocument = iframeElement.contentWindow.document.body;
 
-    // Select elements in iframe
-    const dayHeader = iframeDocument.querySelectorAll("th.PSLEVEL3GRIDODDROW");
-    const rows = iframeDocument.querySelectorAll("table.PSLEVEL3GRIDODDROW  tr");
-    const year = iframeDocument.querySelector("div#win0divDERIVED_CLASS_S_DESCR100_2 td.PSGROUPBOXLABEL.PSLEFTCORNER").textContent;
-    const subjTitleVal = iframeDocument.querySelector('input[name="DERIVED_CLASS_S_SSR_DISP_TITLE$chk"][id="DERIVED_CLASS_S_SSR_DISP_TITLE$chk"]').value;
-    const classInstructorVal = iframeDocument.querySelector('input[name="DERIVED_CLASS_S_SHOW_INSTR$chk"][id="DERIVED_CLASS_S_SHOW_INSTR$chk"]').value;
+        // Select elements in iframe
+        const dayHeader = iframeDocument.querySelectorAll("th.PSLEVEL3GRIDODDROW");
+        const rows = iframeDocument.querySelectorAll("table.PSLEVEL3GRIDODDROW  tr");
+        const year = iframeDocument.querySelector("div#win0divDERIVED_CLASS_S_DESCR100_2 td.PSGROUPBOXLABEL.PSLEFTCORNER").textContent;
+        const subjTitleVal = iframeDocument.querySelector('input[name="DERIVED_CLASS_S_SSR_DISP_TITLE$chk"][id="DERIVED_CLASS_S_SSR_DISP_TITLE$chk"]').value;
+        const classInstructorVal = iframeDocument.querySelector('input[name="DERIVED_CLASS_S_SSR_DISP_ROLE$chk"][id="DERIVED_CLASS_S_SSR_DISP_ROLE$chk"]').value;
 
-    if (!dayHeader || dayHeader.length === 0) {
-        console.error("No day elements found");
-        window.alert("No day elements found");
-        return;
-    }
-
-    if (subjTitleVal === "N" && (selectedEventFormat === "2" || selectedEventFormat === "3")) {
-        console.error('Please check "Show Class Title" box below the calendar under Display Options!');
-        window.alert('Please check "Show Class Title" box below the calendar under Display Options!');
-        return;
-    }
-
-    // Get the dates
-    const days = []
-    const dates = []
-    dayHeader.forEach((element, index) => {
-        if (index === 0) {
-            dates.push("null");
-        } else {
-            const dayText = element.textContent.split("\n");
-            const day = dayText[0].trim();
-            let date = dayText[1]
-            date = addZeroToDate(date);
-            // console.log(`Day: ${day}, Date: ${date}`);
-            days.push(day);
-            dates.push(date);
-            // console.log(`Days: ${days}, Date: ${dates}`);
+        if (!dayHeader || dayHeader.length === 0) {
+            console.error("No day elements found");
+            window.alert("No day elements found");
+            return;
         }
-    });
 
-    // days.shift();
-    // dates.shift();
-    // console.log(days);
-    // console.log(dates);
+        if (subjTitleVal === "N" && (selectedEventFormat === "2" || selectedEventFormat === "3")) {
+            console.error('Please check "Show Class Title" box below the calendar under Display Options!');
+            window.alert('Please check "Show Class Title" box below the calendar under Display Options!');
+            return;
+        }
 
-    // Create array to store all events.
-    let classEvents = [];
-    let skip = createArray(12, 8, 0);
+        // Get the dates
+        const days = []
+        const dates = []
+        dayHeader.forEach((element, index) => {
+            if (index === 0) {
+                dates.push("null");
+            } else {
+                const dayText = element.textContent.split("\n");
+                const day = dayText[0].trim();
+                let date = dayText[1]
+                date = addZeroToDate(date);
+                // console.log(`Day: ${day}, Date: ${date}`);
+                days.push(day);
+                dates.push(date);
+                // console.log(`Days: ${days}, Date: ${dates}`);
+            }
+        });
 
-    // For every tr
-    rows.forEach((row, rowIndex) => {
-        const cells = row.querySelectorAll("td.PSLEVEL3GRIDODDROW");
-        // console.log(`Amount of cells: ${cells.length}`);
+        // days.shift();
+        // dates.shift();
+        // console.log(days);
+        // console.log(dates);
 
-        // track current col skips
-        let curColSkips = 0;
+        // Create array to store all events.
+        let skip = createArray(12, 8, 0);
 
-        // For every cell
-        cells.forEach((cell, susColIndex) => {
-            if (susColIndex > 0) { // Other than the first one
-                let colIndex = susColIndex + curColSkips;
-                // console.log(rowIndex, susColIndex);
+        // For every tr
+        rows.forEach((row, rowIndex) => {
+            const cells = row.querySelectorAll("td.PSLEVEL3GRIDODDROW");
+            // console.log(`Amount of cells: ${cells.length}`);
 
-                if (skip[rowIndex][susColIndex] > 0) {
-                    curColSkips += 1;
-                    colIndex += skip[rowIndex][susColIndex];
-                }
+            // track current col skips
+            let curColSkips = 0;
 
-                try {
-                    const spanElement = cell.querySelector("span");
-                    if (!spanElement) return;
+            // For every cell
+            cells.forEach((cell, susColIndex) => {
+                if (susColIndex > 0) { // Other than the first one
+                    let colIndex = susColIndex + curColSkips;
+                    // console.log(rowIndex, susColIndex);
 
-                    // Get innerHTML and process data
-                    const classContent = spanElement.innerHTML.split('<br>');
-                    let result = procData(classContent, subjTitleVal, classInstructorVal);
-                    // console.log(result);
+                    if (skip[rowIndex][susColIndex] > 0) {
+                        curColSkips += 1;
+                        colIndex += skip[rowIndex][susColIndex];
+                    }
 
-                    const day = days[colIndex - 1];
-                    const startDate = formatDate(dates[colIndex], year);
-                    const endDate = formatDate(dates[colIndex], year);
+                    try {
+                        const spanElement = cell.querySelector("span");
+                        if (!spanElement) return;
 
-                    /*
+                        // Get innerHTML and process data
+                        const classContent = spanElement.innerHTML.split('<br>');
+                        let result = procData(classContent, subjTitleVal, classInstructorVal);
+                        // console.log(result);
+
+                        const day = days[colIndex - 1];
+                        const startDate = formatDate(dates[colIndex], year);
+                        const endDate = formatDate(dates[colIndex], year);
+
+                        /*
                     Let user choose thier own event format:
                     Subject Code - Section (Type)
                     Subject Name - Section (Type)
                     Subject Name - Code - Section (Type)
                     */
-                    let summary = `${result.subjCode} - ${result.classSect} (${result.classType})`;
+                        let summary = `${result.subjCode} - ${result.classSect} (${result.classType})`;
 
-                    if (selectedEventFormat === "2") {
-                        summary = `${result.subjTitle} - ${result.classSect} (${result.classType})`;
-                    } else if (selectedEventFormat === "3") {
-                        summary = `${result.subjTitle} - ${result.subjCode} - ${result.classSect} (${result.classType})`;
-                    }
-
-                    console.log(`Summary: ${summary}, Location: ${result.classLocation}, Day: ${day}, startDateTime: ${startDate}T${result.startTime}, endDateTime: ${endDate}T${result.endTime}`);
-
-                    // If class is 2 hours, mark slot below as "True"
-                    let totalSpan = rowSpan(result.startTime, result.endTime);
-
-                    // If the class's total span is more than an hour
-                    if (totalSpan > 1) {
-                        handleMultiHourClass(totalSpan, rowIndex, skip, colIndex);
-                    }
-
-                    const event = createCalEvent(summary, result.classLocation, startDate, result.startTime
-                        , endDate, result.endTime, selectedSemesterValue, selectedColorValue, selectedReminderTime, selectedOptionValue);
-                    // Append to array after defining events
-                    // console.log('Event: ', event)
-
-                    // classEvents.push(event);
-                    // console.log(classEvents);
-
-                    // Log the selected value
-                    // console.log(`RRULE:FREQ=WEEKLY;COUNT=${selectedSemesterValue}`);
-                    // console.log('Selected semester value:', selectedSemesterValue);
-
-                    if (selectedOptionValue == 1 || selectedOptionValue == 3) {
-                        if (token) {
-                            // console.log("Extension end");
-                            createCalendarEvent(event);
+                        if (selectedEventFormat === "2") {
+                            summary = `${result.subjTitle} - ${result.classSect} (${result.classType})`;
+                        } else if (selectedEventFormat === "3") {
+                            summary = `${result.subjTitle} - ${result.subjCode} - ${result.classSect} (${result.classType})`;
                         }
-                    }
-                    classEvents.push(event);
 
-                } catch (error) {
-                    console.error('Error processing class data:', error);
-                    window.alert('Failed to process class data:', error);
-                    return;
+                        console.log(`Summary: ${summary}, Location: ${result.classLocation}, Day: ${day}, startDateTime: ${startDate}T${result.startTime}, endDateTime: ${endDate}T${result.endTime}`);
+
+                        // If class is 2 hours, mark slot below as "True"
+                        let totalSpan = rowSpan(result.startTime, result.endTime);
+
+                        // If the class's total span is more than an hour
+                        if (totalSpan > 1) {
+                            handleMultiHourClass(totalSpan, rowIndex, skip, colIndex);
+                        }
+
+                        const event = createCalEvent(summary, result.classLocation, startDate, result.startTime
+                            , endDate, result.endTime, selectedSemesterValue, selectedColorValue, selectedReminderTime, selectedOptionValue);
+                        // Append to array after defining events
+                        // console.log('Event: ', event)
+
+                        // classEvents.push(event);
+                        // console.log(classEvents);
+
+                        // Log the selected value
+                        // console.log(`RRULE:FREQ=WEEKLY;COUNT=${selectedSemesterValue}`);
+                        // console.log('Selected semester value:', selectedSemesterValue);
+
+                        if (selectedOptionValue == 1 || selectedOptionValue == 3) {
+                            if (token) {
+                                // console.log("Extension end");
+                                createCalendarEvent(event);
+                            }
+                        }
+                        classEvents.push(event);
+
+                    } catch (error) {
+                        console.error('Error processing class data:', error);
+                        window.alert('Failed to process class data:', error);
+                        return;
+                    }
+                }
+            });
+        });
+    }
+
+    function studentFlow() {
+        // console.log(openClassSec);
+        let classSec = document.querySelectorAll("[id^='win0divDERIVED_SSR_FL_SSR_SCRTAB_DTLS']");
+
+        // For each class sections
+        classSec.forEach((element, index) => {
+            // Select all class name, type, dates, times, and location
+            let className = element.querySelectorAll("[id^='DERIVED_SSR_FL_SSR_SCRTAB_DTLS']");
+            let classDetails = element.querySelectorAll("a[id^='DERIVED_SSR_FL_SSR_SBJ_CAT_NBR$355']");
+            let classDates = element.querySelectorAll("[id^='DERIVED_SSR_FL_SSR_ST_END_DT1']");
+            let classTimes = element.querySelectorAll("[id^='DERIVED_SSR_FL_SSR_DAYSTIMES1']");
+            let classLoc = element.querySelectorAll("[id^='DERIVED_SSR_FL_SSR_DRV_ROOM1']");
+
+            let maxSlots = Math.max(classDetails.length);
+
+            for (let i = 0; i < maxSlots; i++) {
+                // Get the text content of the elements
+                let classNameText = className[0].textContent;
+                let classDetailsText = classDetails[i].textContent.trim();
+                let classDatesText = classDates[i].textContent.trim();
+                let classTimesText = classTimes[i].textContent.trim();
+                let classLocText = classLoc[i].textContent.trim();
+
+                // Call function to ultimately create calendar event
+                groupData(classNameText, classDetailsText, classDatesText, classTimesText, classLocText);
+            }
+        })
+
+        function groupData(className, classDetails, classDates, classTimes, classLoc) {
+            // console.log(className, classType, classDates, classTimes, classLoc);
+
+            let {classCode, classNameOnly} = procClassName(className);
+            let {classType, classSect} = procClassDetails(classDetails);
+            let {startDate, endDate} = procClassDates(classDates);
+            let {startTime, endTime} = procClassTimes(classTimes);
+
+            // console.log(classCode, ",", classNameOnly, ",", classType, ",", classSect, ",", startDate, "-", endDate, ",", startTime, ",", endTime, ",", classLoc);
+
+            /*
+        Let user choose thier own event format:
+        Subject Code - Section (Type)
+        Subject Name - Section (Type)
+        Subject Name - Code - Section (Type)
+        */
+            let summary = `${classCode} - ${classSect} (${classType})`;
+
+            if (selectedEventFormat === "2") {
+                summary = `${classNameOnly} - ${classSect} (${classType})`;
+            } else if (selectedEventFormat === "3") {
+                summary = `${classNameOnly} - ${classCode} - ${classSect} (${classType})`;
+            }
+
+            // console.log("Summary:", summary);
+
+            const event = createCalEvent(summary, classLoc, startDate, startTime
+                , startDate, endTime, selectedSemesterValue, selectedColorValue, selectedReminderTime, selectedOptionValue);
+
+            console.log(event);
+
+            if (selectedOptionValue == 1 || selectedOptionValue == 3) {
+                if (token) {
+                    // console.log("Extension end");
+                    createCalendarEvent(event);
                 }
             }
-        });
-    });
+
+            classEvents.push(event);
+        }
+    }
 
     if (selectedOptionValue == 2 || selectedOptionValue == 3) {
         // Create a blob file for users to download
