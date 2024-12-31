f53b17c15c1460584330a463004c3742846ec997
Kemystra <programmer.coleslaw981@slmail.me>
2024-12-31 21:06:59 +0800

    Fixed failure on exporting ICS when user not logged in to Chrome

--- popup/popup.js
+++ popup/popup.js
@@ -162,8 +162,11 @@ function getFormsValue(selectedOptionValue) {
 // This function handles token and window flow
 async function handleFlow(selectedColorValue, selectedCalendar, selectedReminderTime, selectedSemesterValue, selectedEventFormat, selectedOptionValue) {
     try {
-        // Get Oauth token
-        const token = await getAuthToken();
+        let token = null;
+        if (selectedOptionValue == 1 || selectedOptionValue == 3) {
+            // Get Oauth token
+            token = await getAuthToken();
+        }
 
         // Get the current active tab
         const currTab = await getCurrTab();
